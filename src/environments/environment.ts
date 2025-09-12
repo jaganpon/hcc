ad4145586b4bb4cf35ecf2cfb92d3f6c3dfae366
jagan <jaganpon@gmail.com>
2025-09-12 23:46:29 +0530

    api update in url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  production: false,
-  apiBase: 'https://purple-sky-0ca971600.2.azurestaticapps.net/v1' // change to your backend
+  production: true,
+  apiBase: 'https://purple-sky-0ca971600.2.azurestaticapps.net/api/v1' // change to your backend
 };
